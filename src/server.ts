e3ceacda73387b94a3754ffefab3de94b9f97d58
João Pablo <jp.pablolima@gmail.com>
2026-04-01 07:09:40 -0300

    create controller podcasts-controller

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log( "Hello, world")
+import * as http from  "http";
+import { IncomingMessage, ServerResponse } from "http";
+import { getListEpisodes } from  "./controllers/podcasts-controller";
+
+
+const  server  = http.createServer( async (req:http.IncomingMessage, res:ServerResponse) => {
+    if (req.method ===  "GET") {
+        await getListEpisodes(req, res);
+    }
+});
+
+server.listen(3333, () => {
+    console.log("Servidor iniciado na porta 3333");
+})
